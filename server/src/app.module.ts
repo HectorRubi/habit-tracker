f8d0b7571839951b9d4ea425bf9e4c360c6366b9
Hector Rubi <hector.rubi.garcia@outlook.com>
2026-01-19 16:12:19 -0600

    feat: implement ConfigModule to use env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,17 @@ import { HabitsModule } from './habits/habits.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import configurations from './config/configurations';
 
 @Module({
-  imports: [HabitsModule, CategoriesModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configurations] }),
+    HabitsModule,
+    CategoriesModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/config/environment-variables.ts
@@ -0,0 +1,9 @@
+export interface EnvironmentVariables {
+  database: {
+    host: string;
+    port: number;
+    user: string;
+    password: string;
+    name: string;
+  };
+}
