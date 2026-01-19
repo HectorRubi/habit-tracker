067c5a9044f19848b7232a4ca7a74b9dc193066e
Hector Rubi <hector.rubi.garcia@outlook.com>
2026-01-19 16:23:38 -0600

    feat: implement database connection through TypeOrmModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
 import { HabitsModule } from './habits/habits.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+
 import configurations from './config/configurations';
+import { EnvironmentVariables } from './config/environment-variables';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configurations] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        type: 'postgres',
+        host: configService.get('database.host', { infer: true }),
+        port: configService.get('database.port', { infer: true }),
+        username: configService.get('database.user', { infer: true }),
+        password: configService.get('database.password', { infer: true }),
+        database: configService.get('database.name', { infer: true }),
+        entities: [],
+      }),
+      inject: [ConfigService],
+    }),
     HabitsModule,
     CategoriesModule,
     AuthModule,
