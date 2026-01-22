0e6b86da3feb9a5feb541cda18f3e32e2c51bcee
Hector Rubi <hector.rubi.garcia@outlook.com>
2026-01-21 21:43:07 -0600

    feat: implement a filter to handle all the exceptions

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './utils/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
